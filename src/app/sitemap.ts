39c9f8aa193d20ef57de849e4c52bef9b52094a8
James Rea <jamesrea.work@gmail.com>
2025-01-23 11:28:48 +0000

    Create dynamic sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import { MetadataRoute } from "next";
+import { client } from "@/sanity/lib/client";
+import { sitemapQuery } from "@/sanity/lib/queries/misc";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  try {
+    const paths = await client.fetch(sitemapQuery);
+
+    if (!paths) return [];
+
+    const baseUrl = process.env.VERCEL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000";
+
+      return [
+        {
+          url: baseUrl,
+          lastModified: new Date(),
+          changeFrequency: "weekly",
+          priority: 1,
+        },
+        ...paths.map((path: any) => ({
+          url: new URL(path.href!, baseUrl).toString(),
+          lastModified: new Date(path._updatedAt),
+          changeFrequency: "weekly",
+          priority: 1,
+        })),
+      ];
+  } catch (error) {
+    console.error("Failed to generate sitemap:", error);
+    return [];
+  }
+}
