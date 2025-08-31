8aaeb8d04e7b372e5eb172cede1c03d25c7bd574
Ponsajjan <saju.sajjan@gmail.com>
2025-08-31 22:55:08 +0530

    case study schema change

--- src/components/portable-text/components/stats-section.tsx
+++ src/components/portable-text/components/stats-section.tsx
@@ -1,22 +1,21 @@
-import { PortableText } from '@portabletext/react';
-import { portableTextComponents } from '../portable-text-components';
+
 
 export default function StatsSection({ data }: {
     data: {
         topFullWidthData?: Array<{
-            value: any[];
+            value: string;
             description: string;
         }>;
         leftSideData?: Array<{
-            value: any[];
+            value: string;
             description: string;
         }>;
         rightSideData?: Array<{
-            value: any[];
+            value: string;
             description: string;
         }>;
         bottomFullWidthData?: Array<{
-            value: any[];
+            value: string;
             description: string;
         }>;
     }
@@ -35,9 +34,9 @@ export default function StatsSection({ data }: {
                     {topFullWidthData.map((stat, index) => (
                         <div key={index} className="border-b py-4 lg:pr-8 first:pt-0 border-[#56403833]">
                             <div className="text-3xl font-medium text-[#694D43]">
-                                <PortableText value={stat.value} components={portableTextComponents} />
+                                {stat.value}
                             </div>
-                            <p className="font-normal text-base leading-6 lg:pr-10">
+                            <p className="font-normal text-base leading-6 lg:pr-10 m-0">
                                 {stat.description}
                             </p>
                         </div>
@@ -45,15 +44,15 @@ export default function StatsSection({ data }: {
                 </div>
             )}
             {(leftSideData.length > 0 || rightSideData.length > 0) && (
-                <div className={`md:flex items-center ${bottomFullWidthData.length > 0 ? 'border-b border-[#56403833]' : 'pb-6'}`}>
+                <div className={`md:flex items-center ${bottomFullWidthData.length > 0 ? 'border-b border-[#56403833]' : ''}`}>
                     {leftSideData.length > 0 && (
                         <div className="flex flex-col space-y-4 w-full md:border-r border-[#56403833]">
                             {leftSideData.map((stat, index) => (
                                 <div key={index} className={`border-b pb-4 lg:pr-8 md:last:border-0 border-[#56403833] ${topFullWidthData.length > 0 ? 'first:pt-4' : 'first:pt-0'}`}>
                                     <div className="text-3xl font-medium text-[#694D43]">
-                                        <PortableText value={stat.value} components={portableTextComponents} />
+                                        {stat.value}
                                     </div>
-                                    <p className="font-normal text-base leading-6 lg:pr-10">
+                                    <p className="font-normal text-base leading-6 lg:pr-10 m-0">
                                         {stat.description}
                                     </p>
                                 </div>
@@ -65,9 +64,9 @@ export default function StatsSection({ data }: {
                             {rightSideData.map((stat, index) => (
                                 <div key={index} className="border-b md:pl-7 pb-4 last:border-0 border-[#56403833] first:pt-4 md:first:pt-0">
                                     <div className="text-3xl font-medium text-[#694D43]">
-                                        <PortableText value={stat.value} components={portableTextComponents} />
+                                        {stat.value}
                                     </div>
-                                    <p className="font-normal text-base leading-6 pr-4">
+                                    <p className="font-normal text-base leading-6 pr-4 m-0">
                                         {stat.description}
                                     </p>
                                 </div>
@@ -81,9 +80,9 @@ export default function StatsSection({ data }: {
                     {bottomFullWidthData.map((stat, index) => (
                         <div key={index} className="border-b last:border-b-0 py-4 lg:pr-8 md:last:pb-0 md:last:border-0 border-[#56403833]">
                             <div className="text-3xl font-medium text-[#694D43]">
-                                <PortableText value={stat.value} components={portableTextComponents} />
+                                {stat.value}
                             </div>
-                            <p className="font-normal text-base leading-6 lg:pr-10">
+                            <p className="font-normal text-base leading-6 lg:pr-10 m-0">
                                 {stat.description}
                             </p>
                         </div>
